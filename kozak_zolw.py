1622fa79d562214bdbc5e84195a60a5031597287
Gertyz123 <konradebek@gmail.com>
2018-10-08 06:01:39 +0200

    Now it makes triangle!

--- kozak_zolw.py
+++ kozak_zolw.py
@@ -1,11 +1,18 @@
 import turtle
 
+
+
 def main():
     wn = turtle.Screen()  # creates a graphics window
     alex = turtle.Turtle()  # create a turtle named alex
-    alex.forward(150)  # tell alex to move forward by 150 units
-    alex.left(90)  # turn by 90 degrees
-    alex.forward(75)  # complete the second side of a rectangle
+
+    def triangle():
+        for i in range(3):
+            alex.forward(100)
+            alex.left(120)
+
+    triangle()
+
     turtle.exitonclick()
 
 if __name__ == "__main__":
